93beb2e57a1263f8399330dd44f27bc833084d4c
Grawrb <robmaxfield22@gmail.com>
2024-08-20 21:03:40 -0600

    Commented out files in routes directory due to them becoming redundant with refactoring

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,7 @@
-const router = require('express').Router();
-const userRoutes = require('./user-routes');
+// File commented out as it is no longer necessary due to new routes
+// const router = require('express').Router();
+// const userRoutes = require('./user-routes');
 
-router.use('/users', userRoutes);
+// router.use('/users', userRoutes);
 
-module.exports = router;
+// module.exports = router;
